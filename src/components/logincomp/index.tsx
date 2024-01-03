bbe0cd1946f1a3ce99d617e8f694791aa2b32342
lilphemy <lilphemy@gmail.com>
2024-01-03 15:59:20 +0100

    first file setup for lendsqr

---
+++ src/components/logincomp/index.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import LoginComp from "./logincomponent";
+
+function MainLoginComp () {
+
+    return(
+        <React.Fragment>
+            <h2>I am the login page nigga!</h2>
+            <LoginComp/>
+        </React.Fragment>
+    )
+}
+
+export default MainLoginComp;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/Main.tsx",
+    "./src/App.tsx",
+    "./src/components/logincomp/logincomponent.tsx"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
